f6df85df94f08e4c339197ffdfaad28a41d6b402
mamonov <masevius@rambler.ru>
2015-09-21 17:29:09 +0300

    last 210915

--- lesson3.php
+++ lesson3.php
@@ -1,12 +1,42 @@
 <?php
 
-echo "ceil:".ceil(4).'<br>';
-echo "floor:".  floor(6.345).'<br>';
-echo "round:". round(6.236,2).'<br>';
+$date = array(mt_rand(1, time()),
+    mt_rand(1, time()),
+    mt_rand(1, time()),
+    mt_rand(1, time()),
+    mt_rand(1, time())
+);
 
-mt_srand(time());
-echo mt_rand(10,100);
+echo "Это массив юниксовыми метками <br> ";
+echo var_dump($date);
+
+$show_date = array(date('d.m.Y', $date[0]), //массив с переводом юниксовых меток в даты. Это для наглядности.
+    date('d.m.Y', $date[1]),
+    date('d.m.Y', $date[2]),
+    date('d.m.Y', $date[3]),
+    date('d.m.Y', $date[4]));
+
+echo "Это тот же самый массив, но преобразованный, для наглядности, в даты юниксовыми метками <br> ";
+echo var_dump($show_date);
+
+$show_day = array(date('d', $date[0]), //массив $date с перечнем дней
+    date('d', $date[1]),
+    date('d', $date[2]),
+    date('d', $date[3]),
+    date('d', $date[4]));
+
+$show_month = array(date('m', $date[0]), //массив $date с перечнем месяцев
+    date('m', $date[1]),
+    date('m', $date[2]),
+    date('m', $date[3]),
+    date('m', $date[4]));
+
+echo '<br><br>Наименьший день это - ' . min($show_day) . "<br>";
+echo '<br>Наибольший месяц это - ' . max($show_month) . "<br>";
+
+$sort = sort($date);
+print_r($sort);
 
 
 
-?>
+
