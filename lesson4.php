abdaa405bb5065cf61d17d64c21ad61a47a1f346
mamonov <masevius@rambler.ru>
2015-09-24 14:14:55 +0300

    240915

--- lesson4.php
+++ lesson4.php
@@ -1,10 +1,18 @@
 <?php
-function Test_s(){
-    static $a=1;
-    $a=$a*2;
-    echo $a."<br>";
-}
 
-Test_s();
-echo $a."<br>";
-Test_s();
+echo '<h2>Задание 4 -мое первое подобие интернет-магазина</h2>';
+
+$bear = array('price' => mt_rand(1, 10), 'kol_zak' => mt_rand(1, 10), 'ost' => mt_rand(1, 10), 'discont' => mt_rand(0, 2));
+$jacket = array('price' => mt_rand(1, 10), 'kol_zak' => mt_rand(1, 10), 'ost' => mt_rand(1, 10), 'discont' => mt_rand(0, 2));
+$bike = array('price' => mt_rand(1, 10), 'kol_zak' => mt_rand(1, 10), 'ost' => mt_rand(1, 10), 'discont' => mt_rand(0, 2));
+
+$nazv_price = 'Цена = ';
+$nazv_kolzak = 'Количество заказано = ';
+$nazv_ost = 'Осталось на складе = ';
+$nazv_diskont = 'Discont = ';
+
+echo $nazv_price, $bear['price'] . '<br>';
+echo $nazv_kolzak, $bear['kol_zak'] . '<br>';
+echo $nazv_ost, $bear['ost'] . '<br>';
+echo $nazv_diskont, $bear['discont'] . '<br>';
+
