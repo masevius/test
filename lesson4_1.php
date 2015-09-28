3d5f2b4a37d94a6c83c72fe198d04954ba7c409e
mamonov <masevius@rambler.ru>
2015-09-28 17:38:31 +0300

    280915 1738

--- lesson4_1.php
+++ lesson4_1.php
@@ -1,5 +1,4 @@
 <?php
-
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 ini_set('display_errors', 1);
 
@@ -28,20 +27,70 @@ $bd = parse_ini_string($ini_string, TRUE);
 
 $nazvaniya_tovarov = array_keys($bd); //массив с названиями товаров
 $atrib = array_keys($bd['игрушка мягкая мишка белый']); //массив с атрибутами товаров
-/* print_r($nazvaniya_tovarov);
-  print_r($atrib);
- */
-//print_r($atrib);
-for ($i = 0;
-$i < count($nazvaniya_tovarov); $i++){
-print_r($nazvaniya_tovarov[$i]);
-echo '///////';
-for ($y = 0; $y < count($atrib); $y++) {
-    echo " ";
-    print_r($bd[$nazvaniya_tovarov[$i]][$atrib[$y]]);
-    echo "   ";
-    }
-    echo '<br>';
-}
+var_dump($bd);
 
+function display_atribs($nazvaniya_tovarov, $atrib) {
+    global $bd;
+    global $name;
+    global $price;
+    global $kol_zak;
+    global $ost;
+    global $discont;
+    $name = " ";
+
+    for ($i = 0; $i < count($nazvaniya_tovarov); $i++) {
+        $name .= ($i + 1) . ') ' . $nazvaniya_tovarov[$i] . "<br>";
+    }
+    for ($i = 0; $i < count($nazvaniya_tovarov); $i++) {
+        $price .= $bd[$nazvaniya_tovarov[$i]]['цена'] . "<br>";
+    }
+    for ($i = 0; $i < count($nazvaniya_tovarov); $i++) {
+        $kol_zak .= $bd[$nazvaniya_tovarov[$i]]['количество заказано'] . "<br>";
+    }
+    for ($i = 0; $i < count($nazvaniya_tovarov); $i++) {
+        $ost .= $bd[$nazvaniya_tovarov[$i]]['осталось на складе'] . "<br>";
+    }
+    for ($i = 0; $i < count($nazvaniya_tovarov); $i++) {
+        $discont .= $bd[$nazvaniya_tovarov[$i]]['diskont'] . "<br>";
+    }
+}
 ?>
+<html>
+    <head>
+    </head>
+    <body>
+        <table border="1px"  width="100%" height="20%">
+            <tr bgcolor="#F0FFF0">
+                <th width="25%" align="center">Перечень товаров</th>
+                <th>Цена</th>
+                <th>Кол-во</th>
+                <th>Остаток</th>
+                <th>Скидка</th>
+                <th>Уведомления</th>
+            </tr>
+
+            <tr>
+                <td width="25%"><?
+                    display_atribs($nazvaniya_tovarov, $atrib);
+                    echo $name;
+                    ?></td>
+                <td align="center"><?
+                    echo $price;
+                    ?></td>
+                <td align="center"><?
+                    echo $kol_zak;
+                    ?></td>
+                <td align="center"><?
+                    echo $ost;
+                    ?></td>
+                <td align="center"><?
+                    echo $discont;
+                    ?></td>
+                <td align="center">
+
+                </td>
+        </table>
+        <h1>Итого: <? 
+        ?></h1> Вы заказали 
+    </body>
+</html>
