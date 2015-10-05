c086ef3f8075c02bc574447c1ffdefa2a211548d
mamonov <masevius@rambler.ru>
2015-10-05 15:51:39 +0300

    051015

--- lesson4.php
+++ lesson4.php
@@ -1,66 +1,8 @@
-<?php
-
-error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
-ini_set('display_errors', 1);
-
-echo '<h2>Задание 4 - мое первое подобие интернет-магазина</h2>';
-echo '<h2><br><br>Корзина</h2>';
-echo '<hr>';
-//создание массивов с ценами и т.д
-$bear = array('price' => mt_rand(1, 10),
-    'kol_zak' => mt_rand(1, 10),
-    'ost' => mt_rand(1, 10),
-    'discont' => mt_rand(0, 2));
-$jacket = array('price' => mt_rand(1, 10), 
-    'kol_zak' => mt_rand(1, 10),
-    'ost' => mt_rand(1, 10),
-    'discont' => mt_rand(0, 2));
-$bike = array('price' => mt_rand(1, 10),
-    'kol_zak' => mt_rand(1, 10),
-    'ost' => mt_rand(1, 10), 
-    'discont' => mt_rand(0, 2));
-// расчет итоговой цены по группам товаров
-$total_pice_bear = $bear['price'] * $bear['kol_zak'];
-$total_pice_jacket = $jacket['price'] * $jacket['kol_zak'];
-
-if ($bike['kol_zak'] >= 3) {
-    $total_pice_bike = ($bike['price'] * 0.7 * $bike['kol_zak']);
-    $bike['discont'] = 0;
+<?php 
+function test($x){
+    $x=126;
+    return $x;
 }
-
-function netnasklade($x, $y) { //$x - это переданное количество заказов; $y - сколько осталось на складе
-    if ($x > $y) {
-        echo "К сожалению у нас нет $x  штук, осталось только  $y";
-    }
-}
-
-netnasklade($bear['kol_zak'], $bear['ost']);
-
-function skidki($x, $y) {
-    global $posle_skidki;
-    switch ($y) {
-        case '1':
-            $posle_skidki = $x - $x * 0.1;
-            break;
-        case '2':
-            $posle_skidki = $x - $x * 0.2;
-            break;
-    }
-}
-
-skidki($total_pice_bike, $bike['discont']);
-echo $posle_skidki;
-
-
-echo 'Наименование              | Цена за штуку     |  Количество заказано     |Осталось на складе     |Discont     |Сумма     |<br>';
-echo "игрушка мягкая мишка белый".$bear['price']."       ".$bear['kol_zak']."            ".$bear['ost']."      ".$bear['discont']."      ".$total_pice_bear." ";
-
-//echo $nazv_price, $bear['price'] . '<br>';
-//echo $nazv_kolzak, $bear['kol_zak'] . '<br>';
-//echo $nazv_ost, $bear['ost'] . '<br>';
-//echo $nazv_diskont, $bear['discont'] . '<br>';
-
-
-echo '        При покупке 3 и более велосипедов Вам предоставляется сидка 30%';
-//$bd = parse_ini_string($ini_string, true);
-//print_r($bd);
+$y=1;
+print_r(test($y));
+var_dump(test($y));
---
+++ lesson5.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+//        print_r($_GET);
+        $news = 'Четыре новосибирские компании вошли в сотню лучших работодателей
+Выставка университетов США: открой новые горизонты
+Оценку «неудовлетворительно» по качеству получает каждая 5-я квартира в новостройке
+Студент-изобретатель раскрыл запутанное преступление
+Хоккей: «Сибирь» выстояла против «Ак Барса» в пятом матче плей-офф
+Здоровое питание: вегетарианская кулинария
+День святого Патрика: угощения, пивной теннис и уличные гуляния с огнем
+«Красный факел» пустит публику на ночные экскурсии за кулисы и по закоулкам столетнего здания
+Звезды телешоу «Голос» Наргиз Закирова и Гела Гуралиа споют в «Маяковском»';
+        $news = explode("\n", $news);
+
+//        print_r($news);
+        function whereprint($answer) {
+            global $news, $E_ALL;
+            if ($answer == 'all' || $answer > count($news)) {
+                foreach ($news as $id => $value) {
+                    echo $value;
+                }
+            } else {
+                echo $news[$answer - 1];
+            }
+        }
+
+        whereprint($_GET['id']);
+        ?>
+    </body>
+</html>
