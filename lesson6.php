670c22671c54612e6ee7f5f9601ddb7e47326c0c
mamonov <masevius@rambler.ru>
2015-10-14 16:33:35 +0300

    11

--- lesson6.php
+++ lesson6.php
@@ -77,65 +77,92 @@ function returnID($param) {
 }
 ?>
 <form  method="POST" id="data">
-    <label><input checked <?php checked('private', '1'); ?> type="radio" name="private" value="1" >Частное лицо </label>
-    <label><input  <?php checked('private', '0'); ?> type="radio" name="private" value="0">Компания </label>
+    <table border="1px">       
+        <tr> <label><input checked <?php checked('private', '1'); ?> type="radio" name="private" value="1" >Частное лицо </label>
+        <label><input  <?php checked('private', '0'); ?> type="radio" name="private" value="0">Компания </label>
+        </tr>
+        <tr>
+            <td>Ваше имя<br><br>Электронная почта
+            </td>
+            <td><input type="text"  value="<?php returnID('seller_name') ?>" name="seller_name" >
+                <br><br> <input type="text" class="form-input-text" value="<?php returnID('email') ?>" name="email" id="fld_email">
+            </td>
+            <tr> <br> <input <?php checked('otkaz', '1'); ?> type="checkbox" name="otkaz" value="1">Я не хочу получать вопросы по объявлению на e-mail</tr>
 
-    <br><label for="fld_seller_name" class="form-label"><b id="your-name">Ваше имя</b></label>
-    <input type="text"  value="<?php returnID('seller_name') ?>" name="seller_name" >
+        </tr>
+        <tr>
+            <td>Номер телефона<br><br>Город<br>
+            </td>
+            <td><input type="text"  value="<?php returnID('phone') ?>" name="phone">
+                <br><br> <select title="Выберите Ваш город" name="location_id"> 
+                    <option value="">-- Выберите город --</option>
+                    <option  disabled="disabled">-- Города --</option>
+                    <?php selected('city'); ?>   <br>     
+            </td>
+        </tr>        
+
+    </table>
 
 
-    <br><div class="form-row"> <label for="fld_email" class="form-label">Электронная почта</label>
-        <input type="text" class="form-input-text" value="<?php returnID('email') ?>" name="email" id="fld_email">
-    </div>
+
     <label><input <?php checked('otkaz', '1'); ?> type="checkbox" name="otkaz" value="1">Я не хочу получать вопросы по объявлению на e-mail</label> 
     <div class="form-row"> <label id="fld_phone_label" for="fld_phone" class="form-label">Номер телефона</label>
-        <input type="text" class="form-input-text" value="<?php returnID('phone') ?>" name="phone" id="fld_phone">
+        <table> <table border="1px"> 
+                <tr>
+                    <td>Номер телефона</td>
+                    <td><input type="text" class="form-input-text" value="<?php returnID('phone') ?>" name="phone" id="fld_phone"></td>
+                </tr>
+
+
+            </table>
+
+            <input type="text" class="form-input-text" value="<?php returnID('phone') ?>" name="phone" id="fld_phone">
+
+            <div id="f_location_id" class="form-row form-row-required">
+                <label for="region" class="form-label">Город</label> 
+                <select title="Выберите Ваш город" name="location_id" id="region" class="form-input-select"> 
+                    <option value="">-- Выберите город --</option>
+                    <option class="opt-group" disabled="disabled">-- Города --</option>
+                    <?php selected('city'); ?>        
+                </select> <div id="f_metro_id"> 
+                    <select title="Выберите станцию метро" name="metro_id" id="fld_metro_id" class="form-input-select"> 
+                        <option value="">-- Выберите станцию метро --</option>
+                        <?php selected('metro'); ?>
+
+                    </select> </div> <div id="f_district_id"> 
+
+
+
+                    <div class="form-row"> <label for="fld_category_id" class="form-label">Категория</label> 
+                        <select title="Выберите категорию объявления" name="category_id" id="fld_category_id" class="form-input-select"> 
+                            <option value="">-- Выберите категорию --</option>
+
+                            <?php selected('category'); ?>
+
+
+                            <span class="confirm pseudo-link hidden">Это верный адрес</span>
+                    </div> 
+                    <div class="map-error">Мы не смогли автоматически определить адрес.
+                    </div> 
+                </div> <input type="hidden" disabled="disabled" value="" class="j-address-latitude" name="coords[lat]"> 
+                <input type="hidden" disabled="disabled" value="" class="j-address-longitude" name="coords[lng]">
+                <input type="hidden" disabled="disabled" value="" class="j-address-zoom" name="coords[zoom]"> </div>
+    </div>           
+    <div id="f_title" class="form-row f_title"> <label for="fld_title" class="form-label">Название объявления</label>
+        <input type="text" maxlength="50" class="form-input-text-long" value="<?PHP returnID('title') ?>" name="title" id="fld_title"> </div>
+
+    <div class="form-row"> <label for="fld_description" class="form-label"  id="js-description-label">Описание объявления</label>
+        <textarea maxlength="3000"  name="description" id="fld_description" class="form-input-textarea"><?PHP returnID('description') ?></textarea> </div>
+    <div id="price_rw" class="form-row rl"> <label id="price_lbl" for="fld_price" class="form-label">Цена</label> 
+        <input type="text" maxlength="9" class="form-input-text-short" value="<?PHP returnID('price') ?>" name="price" id="fld_price">&nbsp;<span id="fld_price_title">руб.</span>
+
     </div>
-    <div id="f_location_id" class="form-row form-row-required">
-        <label for="region" class="form-label">Город</label> 
-        <select title="Выберите Ваш город" name="location_id" id="region" class="form-input-select"> 
-            <option value="">-- Выберите город --</option>
-            <option class="opt-group" disabled="disabled">-- Города --</option>
-            <?php selected('city'); ?>        
-        </select> <div id="f_metro_id"> 
-            <select title="Выберите станцию метро" name="metro_id" id="fld_metro_id" class="form-input-select"> 
-                <option value="">-- Выберите станцию метро --</option>
-                <?php selected('metro'); ?>
-
-            </select> </div> <div id="f_district_id"> 
 
 
 
-            <div class="form-row"> <label for="fld_category_id" class="form-label">Категория</label> 
-                <select title="Выберите категорию объявления" name="category_id" id="fld_category_id" class="form-input-select"> 
-                    <option value="">-- Выберите категорию --</option>
-
-                    <?php selected('category'); ?>
-
-
-                    <span class="confirm pseudo-link hidden">Это верный адрес</span>
-            </div> 
-            <div class="map-error">Мы не смогли автоматически определить адрес.
-            </div> 
-        </div> <input type="hidden" disabled="disabled" value="" class="j-address-latitude" name="coords[lat]"> 
-        <input type="hidden" disabled="disabled" value="" class="j-address-longitude" name="coords[lng]">
-        <input type="hidden" disabled="disabled" value="" class="j-address-zoom" name="coords[zoom]"> </div>
-</div>           
-<div id="f_title" class="form-row f_title"> <label for="fld_title" class="form-label">Название объявления</label>
-    <input type="text" maxlength="50" class="form-input-text-long" value="<?PHP returnID('title') ?>" name="title" id="fld_title"> </div>
-
-<div class="form-row"> <label for="fld_description" class="form-label"  id="js-description-label">Описание объявления</label>
-    <textarea maxlength="3000"  name="description" id="fld_description" class="form-input-textarea"><?PHP returnID('description') ?></textarea> </div>
-<div id="price_rw" class="form-row rl"> <label id="price_lbl" for="fld_price" class="form-label">Цена</label> 
-    <input type="text" maxlength="9" class="form-input-text-short" value="<?PHP returnID('price') ?>" name="price" id="fld_price">&nbsp;<span id="fld_price_title">руб.</span>
-
-</div>
-
-
-
-<div class="form-row-indented form-row-submit b-vas-submit" id="js_additem_form_submit">
-    <div class="vas-submit-button pull-left"> <span class="vas-submit-border"></span> <span class="vas-submit-triangle"></span> <input type="submit" value="Далее" id="form_submit" name="main_form_submit" class="vas-submit-input"> </div>
-</div>
+    <div class="form-row-indented form-row-submit b-vas-submit" id="js_additem_form_submit">
+        <div class="vas-submit-button pull-left"> <span class="vas-submit-border"></span> <span class="vas-submit-triangle"></span> <input type="submit" value="Далее" id="form_submit" name="main_form_submit" class="vas-submit-input"> </div>
+    </div>
 </form>
 <?php
 showad();
