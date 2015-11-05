06b342e8c7b2e7efe75841cb606d7584b5d54d4a
mamonov <masevius@rambler.ru>
2015-11-05 16:52:53 +0300

    12

--- 1.php
+++ 1.php
@@ -1,16 +1,42 @@
-<form method="POST">
-    Введите число<br><input type=text" name="inner" > 
-    <select name="oper">
-        <option value="+">+</option>>
-        <option value="-">-</option>>
-    </select>
-    <br><br><input type="submit" value="Отправить">
-</form>
 <?php
-session_start();
-$_SESSION['oper'] = $_POST['oper'];
-$_SESSION[uniqid()] = $_POST['inner'];
-var_dump($_SESSION);
-//session_unset();
+
+function check($par) {
+//    print_r($par);
+    for ($i = 0; $i < count($par); $i++) {
+        if (!is_int($par[$i])) {
+            return FALSE;
+        }
+    }
+}
+
+function tabl() {
+
+    $row = (func_get_arg(0) > func_get_arg(1)) ? func_get_arg(1) : func_get_arg(0);
+    $col = (func_get_arg(0) > func_get_arg(1)) ? func_get_arg(0) : func_get_arg(1);
+
+    $show = '';
+    $chapter = '';
+
+    $chapter = '<tr>';
+    for ($x = 0; $x < $col; $x++) {
+        $chapter .= '<td>' . $x . '<td>';
+    }
+    $chapter = '</tr>';
+    echo $chapter;
+
+    if (func_num_args() <> 2 and check(func_get_args()) == FALSE) {
+        echo 'Нужно ввести 2, причем целых числа.<br> Вы не выполнили одно, а может и сразу два условия!';
+    } else {
+        $show .= '<table border="1px">';
+        $show .= $chapter;
+        for ($i = 0; $i < $row; $i++) {
+            $show .= '<tr>';
+            for ($y = 0; $y < $col; $y++) {
+                $show .= '<td>' . ($y + 1) . '</td>';
+            }$show .= '</tr>';
+        }
+    }echo $show;
+}
+
+tabl(16, 4);
 ?>
-rr
