368a9deb16ceb0f3c6ef6fa63305b7ad474a7213
mamonov <masevius@rambler.ru>
2015-11-05 11:35:06 +0300

    1

--- 1.php
+++ 1.php
@@ -1,10 +1,16 @@
+<form method="POST">
+    Введите число<br><input type=text" name="inner" > 
+    <select name="oper">
+        <option value="+">+</option>>
+        <option value="-">-</option>>
+    </select>
+    <br><br><input type="submit" value="Отправить">
+</form>
 <?php
-
-$greet = function($num) {
-//    
-    echo $num;
- 
-};
-
-$greet('test');
+session_start();
+$_SESSION['oper'] = $_POST['oper'];
+$_SESSION[uniqid()] = $_POST['inner'];
+var_dump($_SESSION);
+//session_unset();
 ?>
+rr
